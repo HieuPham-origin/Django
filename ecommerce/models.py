0957c99cd27b8ab1d6baf591913ecaa2fd1d7235
Hieu Pham-origin <hieupham112003@gmail.com>
2024-01-08 22:23:46 +0700

    database, model, templates

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    role = models.CharField(max_length=20)
+
+class Product(models.Model):
+    pass
